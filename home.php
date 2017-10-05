e83809d36afb862de2a27d28ed26243cde1dd2f8
ceoehis <ceo.ehis@outlook.com>
2017-10-05 17:47:31 +0100

    home page modified

--- home.php
+++ home.php
@@ -1,25 +1,34 @@
 <?php
   include "header.php";
 ?>
-    <div class="top-div">
-      <div id="top">
-        <h1 class="top">Which political party represents your interests?</h1>
+<!-- main -->
+<div class="container-fluid main">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h1>Which political party represents your interests?</h1>
         <p class="top text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum odio enim, blandit quis leo ac, condimentum condimentum eros. Vestibulum eleifend felis ut turpis imperdiet placerat. In egestas elit et faucibus pretium.</p>
-        <button name="submit" type="submit" value="submit" class="btn">TAKE QUIZ</button>
-      </div>
-        <div class="img-wrapper"><img src="./assets/vote.png" alt="where is my image" width="auto" height="auto"></div>
-            
+        <button name="submit" type="submit" value="submit" class="btn btn-lg">TAKE QUIZ</button>
+      </div> 
+      <div class="col-md-6">
+        <!-- <img src="./assets/vote.png" alt="where is my image" width="auto" height="auto"> -->
+      </div>     
     </div>
-    <div class="middle-div">
-      <div >
+  </div>
+</div>
+<!-- end main -->
+<div class="container article">
+  <div class="row">
+    <div class="col-md-6">
         <h1 class="content">Heading 1</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum odio enim, blandit quis leo ac, condimentum condimentum eros. Vestibulum eleifend felis ut turpis imperdiet placerat. In egestas elit et faucibus pretium.</p>
-      </div>
-      <div>
-        <h1 class="content">Heading 2</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum odio enim, blandit quis leo ac, condimentum condimentum eros. Vestibulum eleifend felis ut turpis imperdiet placerat. In egestas elit et faucibus pretium.</p>
-      </div>
     </div>
+    <div class="col-md-6">
+        <h1 class="content">Heading 1</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum odio enim, blandit quis leo ac, condimentum condimentum eros. Vestibulum eleifend felis ut turpis imperdiet placerat. In egestas elit et faucibus pretium.</p>
+    </div>
+    
+  </div>
   
     <?php include ('footer.php'); ?>
 </body>
