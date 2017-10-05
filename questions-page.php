de4d00a30bc522178ad122cde6df5ebbcb4a2e98
ceoehis <ceo.ehis@outlook.com>
2017-10-05 02:36:03 +0100

    update questions.php

--- questions-page.php
+++ questions-page.php
@@ -5,7 +5,8 @@
 <div class="question-container">
         <!-- question section -->
     <h2 class="policy-topic">Issue X</h2>
-    <p class="question">Q3. Do you think Nigeria should diversify her source of revenue?hjsdhklhhhhhhhhhhhhhhh hljksd kljhskjdksjhk skjhskjhds</p>
+    <p class="question">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    quis nostrud exercitation ullamco laboriss?</p>
     <!-- end question -->
     <div class="question-navs">
         <button><< Previous Question</button>
